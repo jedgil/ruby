81553f4fbd601905c2b86b7fbf3838587ebf1b14
Jedediah Gilchrist <jedgilchrist@gmail.com>
2015-08-13 13:20:46 -0700

    improved display and output

--- timbers.rb
+++ timbers.rb
@@ -1,6 +1,12 @@
 system "clear"
 x = 1
-while x < 5
-print "PT! FC!\n"
-x = x + 1
+while x < 35
+  print "PT! FC!\n"
+  x = x + 1
+end
+print "\nPress ENTER to clear the screen."
+clear = gets.chomp
+if clear.empty? 
+  system "clear"
+  else print "wrong move, dirtbag\n\n\n"
 end
