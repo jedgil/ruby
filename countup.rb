883fb912082563bfa57f0544d8ff535d04e07410
Jedediah Gilchrist <jedgilchrist@gmail.com>
2015-08-13 10:44:45 -0700

    first commit

---
+++ countup.rb
@@ -0,0 +1,5 @@
+x = 1
+while x < 41
+  puts x
+  x = x + 1
+end
---
+++ madlib.rb
@@ -0,0 +1,26 @@
+# Get the parts of speech
+print "Please enter an adjective: "
+adj1 = gets.chomp
+print "Please enter another adjective: "
+adj2 = gets.chomp
+print "Please enter a plural noun: "
+plnoun1 = gets.chomp
+print "Please enter a verb: "
+verb1 = gets.chomp
+print "Please enter another verb: "
+verb2 = gets.chomp
+print "Please enter a noun: "
+noun1 = gets.chomp
+print "Please enter an adjective: "
+adj3 = gets.chomp
+print "Please enter a plural noun: "
+plnoun2 = gets.chomp
+print "Please enter an adjective: "
+adj4 = gets.chomp
+print "Please enter a plural noun: "
+plnoun3 = gets.chomp
+print "Please enter a past tense verb: "
+ptverb1 = gets.chomp
+
+# Make the poem.
+print "Twas #{adj1}, and the #{adj2} #{plnoun1}\nDid #{verb1} and #{verb2} in the #{noun1}:\nAll #{adj3} were the #{plnoun2},\nAnd the #{adj4} #{plnoun3} #{ptverb1}.\n"
