ddce985fa3d16c8671b615df893cfd846459c647
Jed Gilchrist <jedgilchrist@gmail.com>
2016-05-25 17:12:03 -0700

    tweaked the syntax of sum.rb

--- sum.rb
+++ sum.rb
@@ -1,12 +1,47 @@
 system "clear"
-print "Pick a number from 1 to 50: "
-a = Integer(gets.chomp)
 
-print "Now pick a number higher than the last one, but still less than 50: "
-b = Integer(gets.chomp)
+def num_a
+  a = 51
+
+  until a <= 50
+    print "Pick a number from 1 to 50: "
+    a = Integer(gets.chomp)
+    puts "Um, try again please." unless a <= 50
+  end
+
+  a
+end
+
+a = num_a
+
+puts "\n"
+
+def num_b
+   b = 51
+
+   until b <= 50
+     print "Now pick a number higher than the last one, but still 50 or less: "
+     b = Integer(gets.chomp)
+     puts "Um, try again please." unless b <= 50
+   end
+
+   b
+ end
+
+b = num_b
+
+=begin
+if b < a || b > 50 then
+  puts "nice try...now this shit won't work"
+end
+=end
+
+puts "\n"
+puts "press enter to see what happens next"
+x = gets.chomp
 
 ary1 = (a..b).to_a
-ary2 = ary1.map { |i| i * 7 }
+ary2 = ary1.map { |i| i**2 }
 
 sum = ary2.reduce(:+)
 
