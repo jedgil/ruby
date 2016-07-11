6bd7580bbb071ac325b939cc470f17617a4e85e3
Jed Gilchrist <jedgilchrist@gmail.com>
2016-07-10 22:40:07 -0700

    segmented methods from output

--- dnd.rb
+++ dnd.rb
@@ -12,8 +12,10 @@ class Character
   def armorclass=(value)
     if value == ""
       raise "Armor Class can't blank!"
-    elsif value > 10
-      raise "Armor Class must be less than 11!"
+#    elsif value > 10
+#      raise "Armor Class must be less than 11!"
+#    else
+#      raise "That number won't do. Moving on."
     end
     @armorclass = value
   end
@@ -21,6 +23,10 @@ class Character
   def strength=(value)
     if value < 1
       raise "Attributes must be greater than 0!"
+#    elsif value > 18
+#      raise "Dice don't work that way!"
+#    else
+#      raise "That number won't do. Moving on."
     end
     @strength = value
   end
@@ -28,10 +34,44 @@ class Character
   def intelligence=(value)
     if value < 1
       raise "Attributes must be greater than 0!"
+#    elsif value > 18
+#      raise "Dice don't work that way!"
+#    else
+#      raise "That number won't do. Moving on."
     end
-    @intelligence
+    @intelligence = value
   end
 end
+
+# I just realized that the methods above are not being called
+# by the inputs below. I need to get that figured out.
+# Trying some sample objects below to see if the logic above works or not.
+
+fighter = Character.new
+fighter.name = "Torvald"
+fighter.armorclass = 5
+fighter.strength = 17
+fighter.intelligence = 12
+
+  puts "Here are a couple of examples:"
+
+  puts ""
+
+  puts "One character is named #{fighter.name} with Armor Class of #{fighter.armorclass}, Strength of #{fighter.strength} and Intelligence of #{fighter.intelligence}."
+
+mage = Character.new
+mage.name = "Merlin"
+mage.armorclass = 7
+mage.strength = 11
+mage.intelligence = 16
+
+  puts ""
+
+  puts "One character is named #{mage.name} with Armor Class of #{mage.armorclass}, Strength of #{mage.strength} and Intelligence of #{mage.intelligence}."
+
+  puts "Press ENTER to continue."
+
+  gets.chomp
 
 system "clear"
 
@@ -39,12 +79,12 @@ puts "Let's make a new character!"
 puts ""
 puts "Give your character a name:"
   @name = gets.chomp
-puts "Rate your character's armor class (the lower the better):"
-  @armorclass = gets.chomp
+puts "Rate your character's Armor Class (the lower the better):"
+  @armorclass = gets.to_i
 puts "Grab some 6-sided dice and roll 3 of them. Add up the total to get your Strength and enter it here:"
-  @strength = gets.chomp
+  @strength = gets.to_i
 puts "Roll again for your Intelligence score and enter it here:"
-  @intelligence = gets.chomp
+  @intelligence = gets.to_i
 puts "Press ENTER to see your stats!"
   gets.chomp
-puts "Your character is named #{@name} with armor class of #{@armorclass}, Strength of #{@strength} and Intelligence of #{@intelligence}."
+puts "Your character is named #{@name} with Armor Class of #{@armorclass}, Strength of #{@strength} and Intelligence of #{@intelligence}."
